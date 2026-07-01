aa3ea92ba1fc3fde83e00dbd0017acc873de5bae
AhmedIbrahim464503 <ab6598282@gmail.com>
2026-07-02 02:29:16 +0500

    frontend: update Next.js build configuration and NPM packages

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
+        destination: 'http://127.0.0.1:8000/api/:path*',
       },
     ]
   },
